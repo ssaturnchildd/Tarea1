404beb9559c9ec06320164e55a4145c4f9bb7b66
jacob <>
2024-10-08 17:42:55 -0600

    Tarea1

---
+++ src/test/java/com/zozmic/Tarea1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zozmic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
